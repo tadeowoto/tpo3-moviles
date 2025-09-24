6a151971d3a1d4e0c0e63010d41cd2c308333fbe
Tadeo Wotoszyn <tadeowotoszyn@MacBook-Pro-de-Tadeo.local>
2025-09-24 19:14:25 -0300

    feat: agregue estilos generales

--- app/src/main/java/com/example/tpo3_productos/ui/agregarProductos/AgregarProductoFragment.java
+++ app/src/main/java/com/example/tpo3_productos/ui/agregarProductos/AgregarProductoFragment.java
@@ -60,7 +60,6 @@ public class AgregarProductoFragment extends Fragment {
     }
 
     private void limpiarCampos(){
-        //Esto puede estar aca profe? no es logica creo...
         binding.inputCodigo.setText("");
         binding.inputDescripcion.setText("");
         binding.inputPrecio.setText("");
